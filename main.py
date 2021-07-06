78e68b823de0dccf735d1f65b14156f45fe6bee7
Erik Drozina <erikdro41@gmail.com>
2021-07-06 15:57:52 +0200

    Added /covid_info
    Added a little free API that tells covid info

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import yaml
+import json
 import logging
+import requests
 from doggo import doggoF
 from pathlib import Path
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
 
 #   retrive the bot token from the token.yaml file
 def get_token():
@@ -17,6 +19,9 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+
+#   needed for /covid_info conversation
+COUNTRY, ISO = range(2)
 
 ########################### BASIC COMM ###########################
 
@@ -38,7 +43,55 @@ def help_command(update: Update, context: CallbackContext) -> None:
 def unknown(update: Update, context: CallbackContext)-> None:
     user = update.message.from_user
     logger.info('User %s started unknow command', user.name)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Unknown command\nMe bot, me no pero like u :(")
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Unknown command\nMe bot, me no pero like u :(')
+
+########################### COVID INFO ###########################
+
+def covid_url(countryname, iso):
+    url = 'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/country-report-iso-based/'+countryname+'/'+iso
+    headers = {
+    'x-rapidapi-key': '0e312021bdmsha51edbc7e3eab41p124c7ajsn40c907167716',
+    'x-rapidapi-host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com'
+    }
+    response = requests.request('GET', url, headers=headers)
+    return response.text
+
+def covid_inf(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info('User %s started /covid_info', user.name)
+    update.message.reply_text('Name of the country (first letter capital)')
+    return COUNTRY
+
+def country(update: Update, context: CallbackContext) -> int:
+    var_country = update.message.text
+    logger.info('Country: %s', var_country)
+    country.country = var_country
+    update.message.reply_text('Acronym of the country (three letters)')
+    return ISO
+
+def iso(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    var_iso = update.message.text
+    logger.info('Acr: %s', var_iso)
+    update.message.reply_text('Loading data...')
+    iso.iso = var_iso
+
+    response = covid_url(country.country, iso.iso).replace("\\'", "")
+    if response != '[]':
+        jres = json.loads(response)
+        update.message.reply_text('Country: '+jres[0]['Country']+', '+jres[0]['ThreeLetterSymbol']+'\nInfection Risk: '+str(jres[0]['Infection_Risk'])+'\nNew Cases: '+str(jres[0]['NewCases'])+'\nNew Recovered: '+str(jres[0]['NewRecovered'])+'\nFatality Rate: '+str(jres[0]['Case_Fatality_Rate']))
+
+    else:
+        update.message.reply_text('No result found, sowwy sar :(')
+    logger.info('User %s ENDED /covid_info', user.name)
+    return ConversationHandler.END
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info('User %s canceled the conversation.', user.first_name)
+    update.message.reply_text('OK, nevermind :)', reply_markup=ReplyKeyboardRemove())
+    logger.info('User %s STOPPED /covid_info', user.name)
+    return ConversationHandler.END
 
 ############################## MAIN ##############################
 
@@ -46,12 +99,22 @@ def main():
     print('Bot Started!\n')
     print('Bot link: http://t.me/erikd_test_bot\n\n')
 
+    covid_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('covid_info', covid_inf)],
+        states={
+            COUNTRY: [MessageHandler(Filters.text & ~Filters.command, country)],
+            ISO: [MessageHandler(Filters.text & ~Filters.command, iso)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
     updater = Updater(get_token(), use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start',start))
     dp.add_handler(CommandHandler('help',help_command))
     dp.add_handler(CommandHandler('doggo',doggoF))
-    dp.add_handler(MessageHandler(Filters.command, unknown)) 
+    dp.add_handler(covid_conv_handler)
+    dp.add_handler(MessageHandler(Filters.command, unknown))
 
     updater.start_polling()
     updater.idle()
